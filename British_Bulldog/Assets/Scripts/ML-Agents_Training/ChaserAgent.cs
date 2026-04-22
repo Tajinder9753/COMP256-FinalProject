2353e473269e3bfdb6c4aeab4e3da555f2da8e98
Tajinder <tnijjar2@my.centennialcollege.ca>
2026-04-21 20:14:30 -0400

    Add Agent Training
    Does get close to the player doesn't always collide with them

--- British_Bulldog/Assets/Scripts/ML-Agents_Training/ChaserAgent.cs
+++ British_Bulldog/Assets/Scripts/ML-Agents_Training/ChaserAgent.cs
@@ -5,41 +5,41 @@ using UnityEngine;
 
 public class ChaserAgent : Agent
 {
-    public Transform Target;
+    public Transform target;
+    public Transform parent;
     public float speed = 10f;
-
-    private Rigidbody rBody;
-
-    public override void Initialize()
-    {
-        rBody = GetComponent<Rigidbody>();
-    }
+    public GameManager gameManager;
+    public Rigidbody rBody;
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        // direction to player
-        sensor.AddObservation(Target.localPosition - transform.localPosition);
+        //training was done in localPosition, so have to convert to local space from world space wasn't working properly otherwise
+        Vector3 agentLocalPosition = parent.InverseTransformPoint(transform.position);
+        Vector3 targetLocalPosition = parent.InverseTransformPoint(target.position);
 
-        // own position
-        sensor.AddObservation(transform.localPosition);
-
-        // velocity
+        sensor.AddObservation(targetLocalPosition - agentLocalPosition);
+        sensor.AddObservation(agentLocalPosition);
         sensor.AddObservation(rBody.linearVelocity.x);
         sensor.AddObservation(rBody.linearVelocity.z);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Vector3 move = new Vector3(
-            actions.ContinuousActions[0],
-            0,
-            actions.ContinuousActions[1]
-        );
-
         rBody.linearVelocity = new Vector3(
-            move.x * speed,
+            actions.ContinuousActions[0] * speed,
             rBody.linearVelocity.y,
-            move.z * speed
+            actions.ContinuousActions[1] * speed
         );
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (gameManager != null)
+            {
+                gameManager.PlayerCaught();
+            }
+        }
     }
 }
